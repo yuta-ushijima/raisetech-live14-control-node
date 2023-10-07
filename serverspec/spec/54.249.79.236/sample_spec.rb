dc727c7bec8cb654318929db3738aec1be1add06
Yuta_Ushizima <contact@yuta-u.com>
2023-10-07 15:08:16 +0900

    fix: describe commandの引数修正

--- serverspec/spec/54.249.79.236/sample_spec.rb
+++ serverspec/spec/54.249.79.236/sample_spec.rb
@@ -4,6 +4,6 @@ describe package('git') do
   it { should be_installed }
 end
 
-describe command('nodenv versions') do
+describe command('source /home/ec2-user/.bash_profile; nodenv versions') do
   its(:stdout) { should match /10\.13\.0/ }
 end
