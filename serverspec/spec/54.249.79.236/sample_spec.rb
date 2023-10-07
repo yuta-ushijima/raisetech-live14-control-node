092fd904c5279849dc1bf718e1f0ef59625bf3e5
Yuta_Ushizima <contact@yuta-u.com>
2023-10-07 12:21:35 +0900

    fix: serverspecのテスト修正

--- serverspec/spec/54.249.79.236/sample_spec.rb
+++ serverspec/spec/54.249.79.236/sample_spec.rb
@@ -4,6 +4,6 @@ describe package('git') do
   it { should be_installed }
 end
 
-describe command('node -v') do
+describe command('nodenv versions') do
   its(:stdout) { should match /10\.13\.0/ }
 end
